29f10e07186a241f977319eefe96ca6db01e038f
emon00007 <skemon152@gmail.com>
2024-07-09 02:43:25 +0600

    nothing added

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
+        {/* <li><a>Item 1</a></li>
         <li>
           <a>Parent</a>
           <ul className="p-2">
@@ -29,14 +29,14 @@ const Navbar = () => {
             <li><a>Submenu 2</a></li>
           </ul>
         </li>
-        <li><a>Item 3</a></li>
+        <li><a>Item 3</a></li> */}
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
+      {/* <li><a>Item 1</a></li>
       <li>
         <details>
           <summary>Parent</summary>
@@ -46,7 +46,7 @@ const Navbar = () => {
           </ul>
         </details>
       </li>
-      <li><a>Item 3</a></li>
+      <li><a>Item 3</a></li> */}
     </ul>
   </div>
   <div className="navbar-end">
