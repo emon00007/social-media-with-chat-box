c15a6984a7e0f306460e46b4b64a397bf7e72e7e
emon00007 <skemon152@gmail.com>
2024-07-11 23:53:35 +0600

    processsing posting site

--- src/Component/Home/HomeMedile.jsx
+++ src/Component/Home/HomeMedile.jsx
@@ -1,10 +1,23 @@
+import { FaImages } from "react-icons/fa";
+
 const HomeMedile = () => {
     return (
         <div>
-        <div>
-            <img src="" alt="" />
-            <input type="text" />
-        </div>
+            <div>
+                <img src="" alt="" />
+                <input type="text" />
+            </div>
+            <hr />
+            <div>
+
+                <div>
+                    <FaImages />
+                    <p>Photo/Video</p>
+                </div>
+                <div>
+                    <p>Feeling/Activity</p>
+                </div>
+            </div>
         </div>
     );
 };
