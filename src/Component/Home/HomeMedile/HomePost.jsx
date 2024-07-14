4eeebfd0d8eba881de8f86f5a241dc1f7707e638
emon00007 <skemon152@gmail.com>
2024-07-14 23:35:07 +0600

    post related

--- src/Component/Home/HomeMedile/HomePost.jsx
+++ src/Component/Home/HomeMedile/HomePost.jsx
@@ -8,8 +8,8 @@ const HomePost = () => {
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title">title</h2>
+                    <p>If a dog chews </p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
