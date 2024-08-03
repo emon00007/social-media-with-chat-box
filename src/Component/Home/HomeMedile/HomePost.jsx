6677f3449f85669a38e17f7c105a20ec872e312c
emon00007 <skemon152@gmail.com>
2024-08-03 15:10:56 +0600

    post showon

--- src/Component/Home/HomeMedile/HomePost.jsx
+++ src/Component/Home/HomeMedile/HomePost.jsx
@@ -1,20 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+
 const HomePost = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => { 
+            try {
+                const res = await axios.get(`http://localhost:5000/PostCollection`);
+                const sortedPosts = res.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+                setPosts(sortedPosts);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+    
+        fetchData();
+    }, []);
+
     return (
         <div className="mx-10">
-            <div className="card  card-compact bg-base-100  shadow-xl">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                        alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">title</h2>
-                    <p>If a dog chews </p>
-                    <div className="card-actions justify-start">
-                        <button className="btn btn-primary">Buy Now</button>
+            {posts.map((post) => (
+                <div key={post.id} className="card card-compact bg-base-100 shadow-xl mb-4">
+                    <figure>
+                        <img
+                            src={post?.image}
+                            alt={post?.caption || "Post Image"} />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{post?.caption}</h2>
+                        <div className="card-actions justify-start">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            ))}
         </div>
     );
 };
